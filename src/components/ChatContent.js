297e4cbab6a77d3db7c8747dcc82b1a662851530
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-03-08 19:14:33 +0800

    fix:  将代码部分用md展示

--- src/components/ChatContent.js
+++ src/components/ChatContent.js
@@ -1,46 +1,39 @@
 import React, { Fragment } from 'react'
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-// import rehypeHighlight from 'rehype-highlight';
-// import { Card } from 'antd'
-// import './ChatContent.css'
+//高亮的主题，还有很多别的主题，可以自行选择
+import { tomorrow, dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-import { Typography, Divider, Space } from 'antd';
-
-const { Title, Paragraph, Text } = Typography;
 function ChatContent(props) {
   const { message } = props
 
-  const renderers = {
-    code: ({ language, value }) => {
-      return <SyntaxHighlighter style={dark} language={language} children={value} />
-    }
-  }
-
   const handleContent = (message) => {
-  //   return  <Typography>
-  //   <Divider />
-  //     <Paragraph>{message}</Paragraph>
-  //   <Divider />
-  //   <Paragraph>
-  //     <Text code>{message}</Text>
-  //   </Paragraph>
-  // </Typography>
-   return <ReactMarkdown renderers={renderers} children={message}></ReactMarkdown>
+    return   <ReactMarkdown
+    children={message}
+    components={{
+      code({node, inline, className, children, ...props}) {
+        const match = /language-(\w+)/.exec(className || '')
+        return !inline && match ? (
+          <SyntaxHighlighter
+            children={String(children).replace(/\n$/, '')}
+            style={tomorrow}
+            language={match[1]}
+            PreTag="div"
+            {...props}
+          />
+        ) : (
+          <code className={className} {...props}>
+            {children}
+          </code>
+        )
+      }
+    }}
+  />
   }
 
   return (
     <Fragment>
       {handleContent(message)}
-      {/* <Card className="content-card">
-        <h2>其它内容</h2>
-        <p>这里是其它内容的描述。</p>
-      </Card>
-      <Card className="code-card">
-        <h2>代码</h2>
-        <pre><code>{data}</code></pre>
-      </Card> */}
     </Fragment>
   )
 }
