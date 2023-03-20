619543d45405e23c875705fa61dad21fa951ff55
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-03-20 20:51:54 +0800

    commit: 支持记录上下文，可以选择记录也可以选择不记录，可以进行配置

--- src/page/ChatPage.js
+++ src/page/ChatPage.js
@@ -1,33 +1,49 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Input, Button, Avatar, message, Drawer, Select, Image, Modal } from 'antd';
+import {
+  Input,
+  Button,
+  Avatar,
+  message,
+  Drawer,
+  Select,
+  Image,
+  Modal,
+} from 'antd';
 import ReactMarkdown from 'react-markdown';
 import { UserOutlined, RobotOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import ChatContent from '../components/ChatContent';
+const { Option } = Select
 const ChatPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
   const [open, setOpen] = useState(false);
-  const [botModel, setBotModel] = useState('chatBot')
-  const [picSize, setPicSize] = useState('512x512')
-  const [picCount, setPicCount] = useState('1')
+  const [botModel, setBotModel] = useState('chatBot');
+  const [picSize, setPicSize] = useState('512x512');
+  const [picCount, setPicCount] = useState('1');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [textToCopy, setTextToCopy] = useState('')
+  const [textToCopy, setTextToCopy] = useState('');
   const chatEndRef = useRef(null);
   const history = useHistory();
 
   // 自动滚动到底部
   useEffect(() => {
     console.log(chatHistory);
-    if(chatEndRef.current) {
+    if (chatEndRef.current) {
       chatEndRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   }, [chatHistory]);
 
+  // 默认是不关联上下文的，节省token
+  useEffect(() => {
+    localStorage.setItem("associationContextLocalStorage", 0);
+  }, [])
+
   const handleOk = () => {
     setIsModalOpen(false);
-     navigator.clipboard.writeText(textToCopy)
+    navigator.clipboard
+      .writeText(textToCopy)
       .then(() => {
         message.success('复制成功');
       })
@@ -49,8 +65,8 @@ const ChatPage = () => {
       history.push('/login');
     }
     if (botModel == 'uiBot') {
-      sendUI()
-      return
+      sendUI();
+      return;
     }
     const newChatHistory = [
       ...chatHistory,
@@ -58,13 +74,26 @@ const ChatPage = () => {
       { sender: 'bot', message: '正在思考中...' },
     ];
     setChatHistory(newChatHistory);
+    const arr = [
+      { sender: 'user', message: '1' },
+      { sender: 'bot', message: '2' },
+    ];
+    console.log(chatHistory);
+
+    let newArr = associationContext();
+    // 不关联上下文我们要将其置为空
+    if (!localStorage.getItem('associationContextLocalStorage')) {
+      newArr = []
+    }
+    // console.log(newArr);
+    // 输出：[{role: 'user', content: '1'}, {role: 'assistant', content: '2'}]
 
     try {
       const response = await axios.post(
         'https://api.openai.com/v1/chat/completions',
         {
           model: 'gpt-3.5-turbo',
-          messages: [{ role: 'user', content: inputValue }],
+          messages: [...newArr, { role: 'user', content: inputValue }],
         },
         {
           headers: {
@@ -88,6 +117,24 @@ const ChatPage = () => {
     setInputValue('');
   };
 
+  // 是否需要进行关联上下文
+  const associationContext = () => {
+    // 进行关联上下文
+    const newArr = chatHistory.map((obj) => {
+      const newObj = { ...obj };
+      if (newObj.sender === 'bot') {
+        newObj.role = 'assistant';
+      } else {
+        newObj.role = newObj.sender;
+      }
+      newObj.content = newObj.message;
+      delete newObj.sender;
+      delete newObj.message;
+      return newObj;
+    });
+    return newArr;
+  };
+
   // 调用接口生成图片
   const sendUI = async () => {
     const newChatHistory = [
@@ -101,9 +148,9 @@ const ChatPage = () => {
       const response = await axios.post(
         'https://api.openai.com/v1/images/generations',
         {
-          "prompt": inputValue,
-          "n": parseInt(picCount),
-          "size": picSize
+          prompt: inputValue,
+          n: parseInt(picCount),
+          size: picSize,
         },
         {
           headers: {
@@ -126,7 +173,7 @@ const ChatPage = () => {
     }
 
     setInputValue('');
-  }
+  };
 
   const showDrawer = () => {
     setOpen(true);
@@ -138,15 +185,15 @@ const ChatPage = () => {
 
   // 切换bot
   const handleChange = (value) => {
-    setBotModel(value)
-  }
+    setBotModel(value);
+  };
 
   // 复制内容
   const copyContent = (event) => {
-    if (botModel === 'uiBot') return
-    setTextToCopy(event.target.textContent)
+    if (botModel === 'uiBot') return;
+    setTextToCopy(event.target.textContent);
     setIsModalOpen(true);
-  }
+  };
 
   return (
     <div
@@ -175,27 +222,46 @@ const ChatPage = () => {
           ]}
         />
         <Select
-        defaultValue="512x512"
-        style={{ width: 150, marginBottom: '15px' }}
-        onChange={(value) => {
-          setPicSize(value)
-        }}
-        options={[
-          { value: '256x256', label: '256x256' },
-          { value: '512x512', label: '512x512' },
-          { value: '1024x1024', label: '1024x1024' },
-        ]}
+          defaultValue="512x512"
+          style={{ width: 150, marginBottom: '15px' }}
+          onChange={(value) => {
+            setPicSize(value);
+          }}
+          options={[
+            { value: '256x256', label: '256x256' },
+            { value: '512x512', label: '512x512' },
+            { value: '1024x1024', label: '1024x1024' },
+          ]}
         />
-        获取图片数量: <Input value={picCount} onChange={(event) => {
-          setPicCount(event.target.value)
-        }}/>
+        获取图片数量:{' '}
+        <Input
+          value={picCount}
+          onChange={(event) => {
+            setPicCount(event.target.value);
+          }}
+        />
+        请选择是否需要进行关联上下文:<Select defaultValue="0" placeholder="请选择是否需要进行关联上下文" 
+          onChange={(value) => {
+            console.log(value)
+            localStorage.setItem("associationContextLocalStorage", value);
+          }}
+        > 
+          <Option value="1">是</Option> 
+          <Option value="0">否</Option> 
+        </Select>
+
       </Drawer>
-      <Modal title="可以进行内容复制" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-         {textToCopy}
+      <Modal
+        title="可以进行内容复制"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {textToCopy}
       </Modal>
       <div
         style={{
-          height: '550px',
+          height: '500px',
           width: '80%',
           marginTop: '50px',
           padding: '20px',
@@ -237,12 +303,18 @@ const ChatPage = () => {
                 }}
                 onClick={copyContent}
               >
-                {message.sender !== 'user' && botModel === 'uiBot' && Array.isArray(message.message) ? (
-                  message && message.message  && message.message.map((item,index) => {
-                    return <Image width={200} src={item.url} key={index} />
+                {message.sender !== 'user' &&
+                botModel === 'uiBot' &&
+                Array.isArray(message.message) ? (
+                  message &&
+                  message.message &&
+                  message.message.map((item, index) => {
+                    return <Image width={200} src={item.url} key={index} />;
                   })
+                ) : Array.isArray(message.message) ? (
+                  '是图片，需要查看请切换到uiBot模式'
                 ) : (
-                  Array.isArray(message.message) ? '是图片，需要查看请切换到uiBot模式' : <ChatContent message={message.message}/>
+                  <ChatContent message={message.message} />
                 )}
               </div>
             </div>
@@ -269,4 +341,4 @@ const ChatPage = () => {
   );
 };
 
-export default ChatPage
+export default ChatPage;
